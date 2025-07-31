72914b8107b5503548b584109a13cc842d3711d8
david amick <github@davidamick.com>
2025-07-31 16:15:49 -0700

    Package sshw, add to shell

--- flake.nix
+++ flake.nix
@@ -4,18 +4,44 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
   outputs = { self, nixpkgs }: let
-    pkgs = nixpkgs.legacyPackages.aarch64-darwin.pkgs;
+    forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+    # https://github.com/yinheli/sshw
+    sshwVersion = "1.1.2";
+    sshw = system: nixpkgs.legacyPackages.${system}.buildGoModule {
+      pname = "sshw";
+      version = sshwVersion;
+      src = nixpkgs.legacyPackages.${system}.fetchFromGitHub {
+        owner = "yinheli";
+        repo = "sshw";
+        rev = "v${sshwVersion}";
+        hash = "sha256-Qr8ICRab85Gse5xb3qECjPNehj86yhvF68l2zSFCG/s=";
+      };
+      vendorHash = "sha256-xI605rup1XjFZj8HH6p3n0mnS1NaVVS2ZMkGuK/QRuY=";
+      meta = with nixpkgs.lib; {
+        description = "SSH client wrapper for automatic login";
+        homepage = "https://github.com/yinheli/sshw";
+        license = licenses.mit;
+      };
+    };
   in
   {
-    devShells.aarch64-darwin.default = pkgs.mkShellNoCC {
-      name = "sshw";
-      buildInputs = with pkgs; [
-        dig
-        icdiff
-        jq
-        just
-        ripgrep
-      ];
-    };
+    packages = forAllSystems (system: {
+      sshw = sshw system;
+    });
+
+    devShells = forAllSystems (system: {
+      default = nixpkgs.legacyPackages.${system}.mkShellNoCC {
+        name = "sshw";
+        buildInputs = with nixpkgs.legacyPackages.${system}; [
+          dig
+          icdiff
+          jq
+          just
+          ripgrep
+          (sshw system)
+        ];
+      };
+    });
   };
 }
